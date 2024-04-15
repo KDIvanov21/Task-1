d4ab89d3a923af846d6d496e8d314ca4d9879ba7
KDIvanov21 <93470215+KDIvanov21@users.noreply.github.com>
2024-04-15 12:14:19 +0300

    Update Task 3 - BankAccount.cpp

--- Task 3 - BankAccount/Task 3 - BankAccount/Task 3 - BankAccount.cpp
+++ Task 3 - BankAccount/Task 3 - BankAccount/Task 3 - BankAccount.cpp
@@ -15,32 +15,32 @@ public:
         balance = initialBalance;
     }
     void displayAccount() {
-        cout << "Client Name: " << clientName << std::endl;
-        cout << "Account Number: " << accountNumber << std::endl;
-        cout << "Balance: " << balance << std::endl;
+        cout << "Client Name: " << clientName << endl;
+        cout << "Account Number: " << accountNumber << endl;
+        cout << "Balance: " << balance << endl;
     }
     void deposit(double amount) {
         balance += amount;
-        cout << "Deposit of " << amount << " successfully completed." << std::endl;
+        cout << "Deposit of " << amount << " successfully completed." << endl;
     }
     void withdraw(double amount) {
         if (balance >= amount) {
             balance -= amount;
-            cout << "Withdrawal of " << amount << " successfully completed." << std::endl;
+            cout << "Withdrawal of " << amount << " successfully completed." << endl;
         }
         else {
-            cout << "Insufficient balance for withdrawal." << std::endl;
+            cout << "Insufficient balance for withdrawal." << endl;
         }
     }
 };
 
 int main() {
     BankAccount account("John Doe", "123456789012345", 1000.0);
-    cout << "Initial Account Details:" << std::endl;
+    cout << "Initial Account Details:" << endl;
     account.displayAccount();
     account.deposit(500.0);
     account.withdraw(200.0);
-    cout << "\nUpdated Account Details:" << std::endl;
+    cout << "\nUpdated Account Details:" << endl;
     account.displayAccount();
     return 0;
 }
